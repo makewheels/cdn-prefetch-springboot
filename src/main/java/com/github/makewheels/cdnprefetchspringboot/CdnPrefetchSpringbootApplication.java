120dcd6713dc526e03ddaf73c72a559cbbfc0b6f
makewheels <makewheels@github.com>
2022-04-05 14:03:39 +0800

    init

---
+++ src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnPrefetchSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cdnprefetchspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnPrefetchSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdnPrefetchSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/cdnprefetchspringboot/CdnPrefetchSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cdnprefetchspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnPrefetchSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
