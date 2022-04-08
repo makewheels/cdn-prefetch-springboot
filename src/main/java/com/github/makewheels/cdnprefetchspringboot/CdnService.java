5ccf2a1f2687880ac2b3304f7a2f8da5b82b1a48
makewheels <makewheels@github.com>
2022-04-08 10:37:31 +0800

    NetUtil.getLocalMacAddress()

--- src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnService.java
+++ src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnService.java
@@ -1,12 +1,14 @@
 package com.github.makewheels.cdnprefetchspringboot;
 
+import cn.hutool.core.net.NetUtil;
 import cn.hutool.http.HttpUtil;
-import com.alibaba.fastjson.JSONArray;
+import cn.hutool.system.oshi.CpuInfo;
+import cn.hutool.system.oshi.OshiUtil;
 import com.alibaba.fastjson.JSONObject;
-import jdk.nashorn.internal.scripts.JO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Currency;
 import java.util.List;
 
 @Service
@@ -21,12 +23,21 @@ public class CdnService {
     }
 
     public JSONObject prefetch(JSONObject prefetch) {
+        String missionId = prefetch.getString("missionId");
+        log.info("收到新任务：missionId = " + missionId);
         new Thread(() -> {
-            String missionId = prefetch.getString("missionId");
-            log.info("missionId = " + missionId);
             List<String> urlList = prefetch.getJSONArray("urlList").toJavaList(String.class);
             download(urlList);
         }).start();
-        return new JSONObject();
+        JSONObject response = new JSONObject();
+        response.put("missionId", missionId);
+        CpuInfo cpuInfo = OshiUtil.getCpuInfo();
+        System.out.println(cpuInfo);
+
+        response.put("message", "我收到任务了,"
+                + " mac = " + NetUtil.getLocalMacAddress()
+                + "\ncpuInfo = " + OshiUtil.getCpuInfo().getCpuModel());
+        return response;
     }
+
 }
