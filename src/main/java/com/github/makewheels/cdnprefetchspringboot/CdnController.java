b609d721bb92e9130b568cc8ddb378ef5bab8986
makewheels <makewheels@github.com>
2022-04-08 11:14:21 +0800

    callbackUrl

--- src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnController.java
+++ src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 
 @RestController
 @RequestMapping("cdn")
--- src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnService.java
+++ src/main/java/com/github/makewheels/cdnprefetchspringboot/CdnService.java
@@ -2,32 +2,49 @@ package com.github.makewheels.cdnprefetchspringboot;
 
 import cn.hutool.core.net.NetUtil;
 import cn.hutool.http.HttpUtil;
+import cn.hutool.system.SystemUtil;
 import cn.hutool.system.oshi.CpuInfo;
 import cn.hutool.system.oshi.OshiUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Currency;
 import java.util.List;
 
 @Service
 @Slf4j
 public class CdnService {
-    private void download(List<String> urlList) {
+    private long download(List<String> urlList) {
+        long totalSize = 0;
         for (String url : urlList) {
             log.info(url);
-            HttpUtil.get(url);
+            byte[] bytes = HttpUtil.createGet(url).execute().bodyBytes();
+            totalSize += bytes.length;
         }
-        log.info("Finished");
+        return totalSize;
     }
 
     public JSONObject prefetch(JSONObject prefetch) {
         String missionId = prefetch.getString("missionId");
         log.info("收到新任务：missionId = " + missionId);
         new Thread(() -> {
+            //计时器
+            long start = System.currentTimeMillis();
             List<String> urlList = prefetch.getJSONArray("urlList").toJavaList(String.class);
-            download(urlList);
+            long totalSize = download(urlList);
+            long time = System.currentTimeMillis() - start;
+
+            JSONObject callbackBody = new JSONObject();
+            callbackBody.put("missionId", missionId);
+            callbackBody.put("totalSize", totalSize);
+            callbackBody.put("count", urlList.size());
+            callbackBody.put("time", time);
+            log.info("Finished, callbackBody = " + callbackBody.toJSONString());
+            HttpUtil.post(prefetch.getString("callbackUrl"), callbackBody.toJSONString());
         }).start();
         JSONObject response = new JSONObject();
         response.put("missionId", missionId);
